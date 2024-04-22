206d5d6afe06e7c46107430d3a3ad6e66e10ceb9
wesly-zhou <weslyz1026@gmail.com>
2024-04-21 22:08:58 -0400

    Put navbar in header.php

--- default.php
+++ default.php
@@ -27,22 +27,7 @@
 </head>
 
 <body onload="funcName()"> 
-    <div class="navbar">
-        <div class="nav_menu">
-            <a class="logo" href="default.php"><img src="" alt="Flight Logo"></a>
-            <a href="explore.php">Explore</a>
-            <a href="flights.php">Flights</a>
-            <a href="stays.php">Stays</a>
-            <a href="events.php">Events</a>
-            <a href="food.php">Food</a>
-            <a href="other.php">Other</a>
-        </div>
-        <div class="hamburger_menu">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-    </div>
+    <?php include 'header.php'; ?>
 
         <?php
             // Establish connection info to database one
@@ -68,5 +53,6 @@
 
             $conn->close();
         ?>
+    <?php include 'footer.php'; ?>
 </body>
 </html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<div class="navbar">
+    <div class="nav_menu">
+        <a class="logo" href="default.php"><img src="" alt="Flight Logo"></a>
+        <a href="explore.php">Explore</a>
+        <a href="flights.php">Flights</a>
+        <a href="stays.php">Stays</a>
+        <a href="events.php">Events</a>
+        <a href="food.php">Food</a>
+        <a href="other.php">Other</a>
+    </div>
+    <div class="hamburger_menu">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</div>
