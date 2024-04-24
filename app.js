712a7dedec95ea0a7c76c8b5ec6a116dc941b108
AKaspal <111235859+WhatItDo22@users.noreply.github.com>
2024-04-24 16:14:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-
 let map;
 let directionsService;
 let directionsRenderer;
-let markers = []; // Array to store the markers
-let waypointCount = 0; // Counter for generating unique IDs
+let markers = [];
+let waypointCount = 0;
 
 window.onload = function() {
   initMap();
@@ -12,13 +11,12 @@ window.onload = function() {
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 6,
-    center: { lat: 37.1, lng: -95.7 } // Centered on the US
+    center: { lat: 37.1, lng: -95.7 }
   });
 
   directionsService = new google.maps.DirectionsService();
   directionsRenderer = new google.maps.DirectionsRenderer({ map: map });
 
-  // Initialize autocomplete for initially present inputs
   setupAutocomplete('origin');
   setupAutocomplete('destination');
 }
@@ -35,10 +33,10 @@ function addWaypoint() {
   input.type = 'text';
   input.placeholder = 'Enter waypoint';
   input.className = 'waypoint';
-  input.id = `waypoint-${waypointCount}`; // Generate a unique ID for each waypoint
+  input.id = `waypoint-${waypointCount}`;
   container.insertBefore(input, container.lastChild);
-  setupAutocomplete(input.id); // Setup autocomplete for the new input
-  waypointCount++; // Increment the counter
+  setupAutocomplete(input.id);
+  waypointCount++;
 }
 
 function calculateAndDisplayRoute() {
@@ -157,3 +155,155 @@ function clearPreviousResults() {
   markers = [];
   directionsRenderer.setDirections({ routes: [] });
 }
+
+var page = 0;
+var ticketMatsterWidgetTemplate = document.getElementById('Ticketmaster-widget').outerHTML;
+var searchButton = $(".button");
+var cityI= "Chicago";
+var stateI = "IL";
+var Today = moment().format('YYYY-MM-DD');
+var dateI = Today;
+var categoryI= "";
+var Family="yes";
+var TktAPIKey = "MRyvwbGL4H4yvINfi4pGByvFdAPc4yrC";
+
+searchButton.on("click", function() {
+  FamilyorNot();
+  selectQuery();
+  getEvents(page);
+  reloadTicketmasterWidget();
+});
+
+function selectQuery() {
+  cityI = $('#City').val();
+  stateI = $('#state').val(); 
+  dateI = $('#eventDate').val();
+  console.log(' City entered: ' + cityI);
+  console.log(' State entered: ' + stateI);
+  console.log(' Date entered: ' + dateI);
+  console.log("Show family events only is " + Family);
+}
+
+function FamilyorNot() {
+  if(document.getElementById('showFamily').checked) {
+    Family="only";
+    console.log(" Only family events is checked so mark Family as " + Family);
+  }
+  else if(document.getElementById('over21').checked) {
+    Family="no";
+    console.log("Over 21 is checked so mark it as " + Family);
+  }
+  else {
+    Family="yes";
+    console.log("Radio button was not used so both Family and other events must show : " + Family);
+  }
+}
+
+function getEvents(page) {
+  $('#events-panel').show();
+  $('#attraction-panel').hide();
+
+  if (page < 0) {
+    page = 0;
+    return;
+  }
+  if (page > 0) {
+    if (page > getEvents.json.page.totalPages-1) {
+      page=0;
+      return;
+    }
+  }
+ 
+  $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey="+TktAPIKey+"&sort=date,asc"+"&city="+cityI+"&countryCode=US"+"&state="+stateI+"&startedatetime="+dateI+"&classificationName="+categoryI+"&includeFamily="+Family+"&size=4&page="+page,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+          getEvents.json = json;
+          showEvents(json);
+          console.log(json);
+         },
+    error: function(xhr, status, err) {
+  			  console.log(err);
+  		   }
+  });
+}
+
+function showEvents(json) {
+  var items = $('#events .list-group-item');
+  items.hide();
+  var events = json._embedded.events;
+  var item = items.first();
+  for (var i=0;i<events.length;i++) {
+    item.children('.list-group-item-heading').text(events[i].name);
+    item.children('.list-group-item-text').text(events[i].dates.start.localDate);
+    try {
+      item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
+    } catch (err) {
+      console.log(err);
+    }
+    item.show();
+    item.off("click");
+    item.click(events[i], function(eventObject) {
+      console.log(eventObject.data);
+      try {
+        getAttraction(eventObject.data._embedded.attractions[0].id);
+      } catch (err) {
+      console.log(err);
+      }
+    });
+    item=item.next();
+  }
+}
+
+$('#prev').click(function() { 
+  getEvents(--page);
+});
+
+$('#next').click(function() {
+  getEvents(++page);
+});
+
+function getAttraction(id) {
+  $.ajax({
+    type:"GET",
+    url:"https://app.ticketmaster.com/discovery/v2/attractions/"+id+".json?apikey="+TktAPIKey,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+          showAttraction(json);
+  		   },
+    error: function(xhr, status, err) {
+  			  console.log(err);
+  		   }
+  });
+}
+
+function showAttraction(json) {
+  $('#events-panel').hide();
+  $('#attraction-panel').show();
+  
+  $('#attraction-panel').click(function() {
+    getEvents(page);
+  });
+  
+  $('#attraction .list-group-item-heading').first().text(json.name);
+  $('#attraction img').first().attr('src',json.images[0].url);
+  $('#classification').text(json.classifications[0].segment.name + " - " + json.classifications[0].genre.name + " - " + json.classifications[0].subGenre.name);
+  console.log(json.classifications[0].genre.name);
+}
+
+function reloadTicketmasterWidget() {
+  $('#Ticketmaster-widget').fadeOut(400, function() {
+    var newTemplate = $(ticketMatsterWidgetTemplate);
+    newTemplate.attr('w-city', cityI, 'w-state', stateI);
+    $('#Ticketmaster-widget').html(newTemplate);
+    var s = document.createElement('script');
+    s.src = 'https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js';
+    document.body.appendChild(s);
+    $('#Ticketmaster-widget').fadeIn(400);
+  });
+}
+
+getEvents(page);
