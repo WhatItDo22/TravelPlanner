420dc8f076c14cbaf1a5e051fe8ca6c013126f5c
wesly-zhou <weslyz1026@gmail.com>
2024-04-25 14:34:15 -0400

    Database info for users in config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+    $server = "localhost";
+    $username = "upjomg4jsiwwg";
+    $password = "533%3611n_4`";
+    $db = "dbz0xs4h1mcple";
+    $conn = new mysqli($server, $username, $password);
