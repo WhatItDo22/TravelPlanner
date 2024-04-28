474942169cacb8f9c0068cb4a0f9050f134d0650
wesly-zhou <weslyz1026@gmail.com>
2024-04-28 02:01:59 -0400

    Header for all pages

--- header.php
+++ header.php
@@ -1,5 +1,16 @@
 <?php include 'check.php'; ?>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="faqstyles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    <title><?php echo $title; ?></title>
+</head>
 
+<body> 
 <?php if ($loginst == 0) { ?>
     <div class="navbar">
         <a class="logo" href="default.php" id="home"><img src="images/logo.png" alt="Travel Logo"></a>
