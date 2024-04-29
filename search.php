4152080c06111722a199bfc33532bf4e7c0ac0a9
ncampo01 <157998211+ncampo01@users.noreply.github.com>
2024-04-29 04:42:04 -0400

    Update search.php

--- search.php
+++ search.php
@@ -9,121 +9,119 @@
             <div class="row">
                 <div class="events-col-1">
                     <img src="images/Road_Trip_Illustration.jpg" alt="car facing away from the user and towards the sun">
-                </div>
-                <div class="events-col-2">
-                    <h2 class="sub-title">What would <strong style="color: #ADEFD1;">YOU</strong> like to look at?</h2>
-                    <p>Choose from either three to view listings!</p>
-                    <div class="tab-titles">
-                        <p class="tab-links active-link" onclick="opentab('events')">Events</p>
-                        <p class="tab-links" onclick="opentab('hotels')" >Hotel</p>
-                        <p class="tab-links" onclick="opentab('restaurants')">Restaurant</p>
                     </div>
-      
-                    <!---------- ticketmaster ------------->
-                    <div class="tab-contents active-tab" id="events">
-                        <section class="events-container">
-                        <div class="small--container">
-                            <h2>Events</h2>
-                            <div class="search-form">
-                                <input id="City" type="text" placeholder="Enter city">
-                                <?php
-                                    $server = "localhost";
-                                    $username = "upjomg4jsiwwg";
-                                    $password = "533%3611n_4`";
-                                    $db = "dbxr6uvbrsv2dg";
-                                    $conn = new mysqli($server, $username, $password);
-                                    if ($conn->connect_error) {
-                                        die("Connection failed: " . $conn->connect_error);
-                                    }
-                                    $conn->select_db($db);
-                                    $sql = "SELECT Name, Abbreviation FROM States";
-                                    $result = $conn->query($sql);
-                                    echo "<select name='state' id='state'>";
-                                    if ($result->num_rows > 0) {
-                                        while($row = $result->fetch_assoc()) {
-                                            echo "<option value='{$row['Abbreviation']}'>{$row['Name']}</option>";
-                                        }
-                                    }
-                                    echo "</select>";
-                                    $conn->close();
-                                ?>
-                                <input type="date" id="eventDate">
-                                <button class="button">Search</button>
-                            </div>
-                            <div id="events-panel">
-                                <div class="panel panel-primary">
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title">Events</h3>
+                    <div class="events-col-2">
+                        <h2 class="sub-title">What would <strong style="color: #ADEFD1;">YOU</strong> like to look at?</h2>
+                        <p>Choose from either three to view listings!</p>
+                        <div class="tab-titles">
+                            <p class="tab-links active-link" onclick="opentab('events')">Events</p>
+                            <p class="tab-links" onclick="opentab('hotels')" >Hotel</p>
+                            <p class="tab-links" onclick="opentab('restaurants')">Restaurant</p>
+                        </div>
+        
+                        <!---------- ticketmaster ------------->
+                        <div class="tab-contents active-tab" id="events">
+                            <section class="events-container">
+                                <div class="small--container">
+                                    <h2>Events</h2>
+                                    <div class="search-form">
+                                        <input id="City" type="text" placeholder="Enter city">
+                                        <?php
+                                            $server = "localhost";
+                                            $username = "upjomg4jsiwwg";
+                                            $password = "533%3611n_4`";
+                                            $db = "dbxr6uvbrsv2dg";
+                                            $conn = new mysqli($server, $username, $password);
+                                            if ($conn->connect_error) {
+                                                die("Connection failed: " . $conn->connect_error);
+                                            }
+                                            $conn->select_db($db);
+                                            $sql = "SELECT Name, Abbreviation FROM States";
+                                            $result = $conn->query($sql);
+                                            echo "<select name='state' id='state'>";
+                                            if ($result->num_rows > 0) {
+                                                while($row = $result->fetch_assoc()) {
+                                                    echo "<option value='{$row['Abbreviation']}'>{$row['Name']}</option>";
+                                                }
+                                            }
+                                            echo "</select>";
+                                            $conn->close();
+                                        ?>
+                                        <input type="date" id="eventDate">
+                                        <button class="button">Search</button>
                                     </div>
-                                    <div class="panel-body">
-                                        <div id="events" class="list-group">
-                                            <a href="#" class="list-group-item">
-                                                <h4 class="list-group-item-heading"></h4>
-                                                <p class="list-group-item-text"></p>
-                                                <p class="venue"></p>
-                                            </a>
+                                    <div id="events-panel">
+                                        <div class="panel panel-primary">
+                                            <div class="panel-heading">
+                                                <h3 class="panel-title">Events</h3>
+                                            </div>
+                                            <div class="panel-body">
+                                                <div id="events" class="list-group">
+                                                    <a href="#" class="list-group-item">
+                                                        <h4 class="list-group-item-heading"></h4>
+                                                        <p class="list-group-item-text"></p>
+                                                        <p class="venue"></p>
+                                                    </a>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
+                                    <div class="pagination">
+                                        <button id="prev">Previous</button>
+                                        <button id="next">Next</button>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="pagination">
-                                <button id="prev">Previous</button>
-                                <button id="next">Next</button>
-                            </div>
+                            </section>
                         </div>
-                        </section>
-                    <div class="small--container">
-                        <div id="attraction-panel">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title" id="attraction">Attraction</h3>
-                                </div>
-                                <div id="attraction" class="panel-body">
-                                    <h4 class="list-group-item-heading">Attraction title</h4>
-                                        <img class="col-xs-12" src="">
-                                        <p id="classification" style="margin-bottom=5px;"></p>
+                        <div class="small--container">
+                            <div id="attraction-panel">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        <h3 class="panel-title" id="attraction">Attraction</h3>
+                                    </div>
+                                    <div id="attraction" class="panel-body">
+                                        <h4 class="list-group-item-heading">Attraction title</h4>
+                                            <img class="col-xs-12" src="">
+                                            <p id="classification" style="margin-bottom=5px;"></p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         <div id="Ticketmaster-widget"></div>
-                    </div>
-                </div>
         
-        <!---------- hotels ------------->
-        <div class="tab-contents" id="hotels">
-              <section class="hotel-search-container">
-                    <div class="small--container">
-                        <h2>Hotel Search</h2>
-                        <div class="search-form">
-                            <input id="autocomplete" type="text" placeholder="Enter a city">
-                            <button id="search-button">Search</button>
+                        <!---------- hotels ------------->
+                        <div class="tab-contents" id="hotels">
+                            <section class="hotel-search-container">
+                                    <div class="small--container">
+                                        <h2>Hotel Search</h2>
+                                        <div class="search-form">
+                                            <input id="autocomplete" type="text" placeholder="Enter a city">
+                                            <button id="search-button">Search</button>
+                                        </div>
+                                        <div id="map"></div>
+                                        <div id="hotel-results">
+                                            <table id="results"></table>
+                                        </div>
+                                    </div>
+                            </section>
                         </div>
-                        <div id="map"></div>
-                        <div id="hotel-results">
-                            <table id="results"></table>
+                        
+                        <!---------- restaurants ------------->
+                        <div class="tab-contents" id="restaurants">
+                            <section class="restaurant-search-container">
+                                <div class="small--container">
+                                    <h2>Restaurant Search</h2>
+                                    <div class="search-form">
+                                        <input id="restaurant-location" type="text" placeholder="Enter a location">
+                                        <input id="restaurant-query" type="text" placeholder="Enter a cuisine type (optional)">
+                                        <button id="restaurant-search-button">Search</button>
+                                    </div>
+                                    <div id="restaurant-map"></div>
+                                    <div id="restaurant-results">
+                                        <table id="restaurant-table"></table>
+                                    </div>
+                                </div>
+                            </section>
                         </div>
-                    </div>
-              </section>
-        </div>
-        
-        <!---------- restaurants ------------->
-        <div class="tab-contents" id="restaurants">
-            <section class="restaurant-search-container">
-                <div class="small--container">
-                    <h2>Restaurant Search</h2>
-                    <div class="search-form">
-                        <input id="restaurant-location" type="text" placeholder="Enter a location">
-                        <input id="restaurant-query" type="text" placeholder="Enter a cuisine type (optional)">
-                        <button id="restaurant-search-button">Search</button>
-                    </div>
-                    <div id="restaurant-map"></div>
-                        <div id="restaurant-results">
-                            <table id="restaurant-table"></table>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            </div>
             </div>
         </div>
     </div>
