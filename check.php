94fabc2bf4e6dc2c6b3eceaca95f4134ced17723
wesly-zhou <weslyz1026@gmail.com>
2024-04-28 23:01:49 -0400

    Revert check.php

--- check.php
+++ check.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
 $loginst = 0;
-var_dump($_SESSION["user"]);
 if (isset($_SESSION["user"])) {
     $loginst = 1;
-}
-if (isset($_SESSION["test"])) {
-    $loginst = 3;
 }
 ?>
