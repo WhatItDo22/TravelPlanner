b01d034b42455246e9e208ba9bcdee1a11b30193
wesly-zhou <weslyz1026@gmail.com>
2024-04-29 07:21:35 -0400

    style

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
     </div>
 <?php } else { ?>
     <div class="navbar">
-        <a class="logo" href="default.php" id="home"><img src="images/logo.png" alt="Travel Logo"></a>
+        <a class="logo" href="default.php" id="home"><img src="images/logo.jpg" alt="Travel Logo"></a>
         <div class="nav_menu">
             <a href="search.php" id="search">Search</a>
             <a href="faq.php" id="faq">FAQ</a>
