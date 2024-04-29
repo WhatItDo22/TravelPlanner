becfc75be827346ec81a99486acd547dc230cd5d
AKaspal <111235859+WhatItDo22@users.noreply.github.com>
2024-04-29 03:48:22 -0400

    Update default.php

--- default.php
+++ default.php
@@ -1,25 +1,32 @@
 <?php
-    session_start();
-    $title = "ItineraEase";
+session_start();
+$title = "ItineraEase";
 ?>
-    <?php include 'header.php'; ?>
-    <div class="container">
-        <div id="controlPanel">
-            <input id="origin" type="text" placeholder="Enter origin" class="waypoint input-fixed-width">
-            <input id="destination" type="text" placeholder="Enter destination" class="waypoint input-fixed-width">
-            <input id="poiType" type="text" placeholder="Enter types of places (e.g., museum, restaurant)" class="input-fixed-width">
-      </div>
+
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <div id="controlPanel">
+        <input id="origin" type="text" placeholder="Enter origin" class="waypoint input-fixed-width">
+        <input id="destination" type="text" placeholder="Enter destination" class="waypoint input-fixed-width">
+        <input id="poiType" type="text" placeholder="Enter types of places (e.g., museum, restaurant)" class="input-fixed-width">
+    </div>
 
     <div id="dynamicWaypointsContainer"></div>
-        <div id="waypointsContainer">
-            <button onclick="addWaypoint()" id="waypoint">Add Waypoint</button>
-            <button onclick="calculateAndDisplayRoute()" id="submit__button">Get Directions</button>
-        </div>
+
+    <div id="waypointsContainer">
+        <button onclick="addWaypoint()" id="waypoint">Add Waypoint</button>
+        <button onclick="calculateAndDisplayRoute()" id="submit_button">Get Directions</button>
     </div>
-    <div id="mapContainer">
-        <div id="map"></div>
-    </div>
-    <?php include 'footer.php'; ?>
-    <script src="app.js"></script>
+</div>
+
+<div id="mapContainer">
+    <div id="map"></div>
+    <button id="addRouteButton">Add route</button>
+</div>
+
+<?php include 'footer.php'; ?>
+
+<script src="app.js"></script>
 </body>
 </html>
