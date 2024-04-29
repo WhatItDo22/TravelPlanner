f0aaf973161528857bd12de9f38dc12c0bd4de95
wesly-zhou <weslyz1026@gmail.com>
2024-04-29 06:46:08 -0400

    Update header.php

--- header.php
+++ header.php
@@ -11,8 +11,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/TravelPlanner/styles.css">
-    <link rel="stylesheet" href="/TravelPlanner/<?php echo $style; ?>">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="<?php echo $style; ?>">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhUuWak2CTtiOWi0ycSLTJU43cJVch2_w&libraries=places"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
