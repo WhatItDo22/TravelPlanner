e9e1734ec95c2bc084a29e48cbcc46e83449d371
wesly-zhou <weslyz1026@gmail.com>
2024-04-28 22:11:03 -0400

    Changed navbar

--- header.php
+++ header.php
@@ -12,14 +12,20 @@
 </head>
 
 <body> 
-<?php if ($loginst == 0) { ?>
+<?php if ($loginst == 1) { ?>
     <div class="navbar">
         <a class="logo" href="default.php" id="home"><img src="images/logo.png" alt="Travel Logo"></a>
         <div class="nav_menu">
             <a href="search.php" id="search">Search</a>
             <a href="faq.php" id="faq">FAQ</a>
             <a href="contact.php" id="contact">Contact</a>
-            <a href="login.php" id="login">Log In</a>
+            <div class="dropdown">
+                <a onclick="myFunction()" class="dropbtn">Account</a>
+                <div id="myDropdown" class="dropdown-content">
+                    <a href="savedtrips.php">Saved Trips</a>
+                    <a href="logout.php">Log Out</a>
+                </div>
+            </div>
         </div>
         <div class="hamburger_menu">
             <span></span>
@@ -34,13 +40,7 @@
             <a href="search.php" id="search">Search</a>
             <a href="faq.php" id="faq">FAQ</a>
             <a href="contact.php" id="contact">Contact</a>
-            <div class="dropdown">
-                <a onclick="myFunction()" class="dropbtn">Account</a>
-                <div id="myDropdown" class="dropdown-content">
-                    <a href="savedtrips.php">Saved Trips</a>
-                    <a href="logout.php">Log Out</a>
-                </div>
-            </div>
+            <a href="login.php" id="login">Log In</a>
         </div>
         <div class="hamburger_menu">
             <span></span>
