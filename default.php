9b77a5ec5f5cf23e4ba5aa902681b189b89aedb8
wesly-zhou <weslyz1026@gmail.com>
2024-04-29 22:05:07 -0400

    Update to default

--- default.php
+++ default.php
@@ -50,17 +50,16 @@
                     $sql2 = "INSERT INTO waypoints (tripID, latitude, longitude)
                     VALUES ('$tripID', $coordinates.lat, $coordinates.lng)";
                 }
-                if ($conn->query($sql) === TRUE) {
+                if ($conn->query($sql2) === TRUE) {
                     echo "New records created successfully";
                 } else {
-                    echo "Error: " . $sql . "<br>" . $conn->error;
+                    echo "Error: " . $sql2 . "<br>" . $conn->error;
                 }
             }
         ?>
         <form method="post">
             <input type="submit" class="btn1" name="btn-route" value="Add Route">
         </form>
-        <?php print_r($_REQUEST) ?>
     </div>
 </div>
 
