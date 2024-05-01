17460dad724eb2c43d439581a2cde8b8009f19e6
AKaspal <111235859+WhatItDo22@users.noreply.github.com>
2024-05-01 06:41:41 -0400

    Update savedtrips.php

--- savedtrips.php
+++ savedtrips.php
@@ -41,7 +41,7 @@ $conn->close();
 <?php
 $locations = array();
 if (isset($_POST['btn-route'])) {
-    $tripID = 1;
+    $tripID = $_POST['trip_route'];
     $username = $user["username"];
     $server = 'localhost';
     $dbusername = 'upjomg4jsiwwg';
@@ -64,7 +64,7 @@ if (isset($_POST['btn-route'])) {
     $conn->close();
 }
 ?>
-<script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhUuWak2CTtiOWi0ycSLTJU43cJVch2_w&libraries=places"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhUuWak2CTtiOWi0ycSLTJU43cJVch2_w&libraries=places"></script>
 <script type="text/javascript">
 var map;
 var Markers = {};
@@ -78,36 +78,45 @@ var locations = [
 <?php echo $i; ?>
 ]<?php if($j!=sizeof($locations))echo ","; }?>
 ];
-var origin = new google.maps.LatLng(locations[0][1], locations[0][2]);
+
 function initialize() {
-var mapOptions = {
-zoom: 9,
-center: origin
-};
-map = new google.maps.Map(document.getElementById('map'), mapOptions);
-infowindow = new google.maps.InfoWindow();
-for(i=0; i<locations.length; i++) {
-var position = new google.maps.LatLng(locations[i][1], locations[i][2]);
-var marker = new google.maps.Marker({
-position: position,
-map: map,
-});
-google.maps.event.addListener(marker, 'click', (function(marker, i) {
-return function() {
-infowindow.setContent(locations[i][0]);
-infowindow.open(map, marker);
+    if (locations.length > 0) {
+        var origin = new google.maps.LatLng(locations[0][1], locations[0][2]);
+        var mapOptions = {
+            zoom: 9,
+            center: origin
+        };
+        map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        infowindow = new google.maps.InfoWindow();
+        for (i = 0; i < locations.length; i++) {
+            var position = new google.maps.LatLng(locations[i][1], locations[i][2]);
+            var marker = new google.maps.Marker({
+                position: position,
+                map: map,
+            });
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+            Markers[locations[i][3]] = marker;
+        }
+        locate(0);
+    } else {
+        console.log("No locations found.");
+    }
 }
-}) (marker, i));
-Markers[locations[i][3]] = marker;
-}
-locate(0);
-}
+
 function locate(marker_id) {
-var myMarker = Markers[marker_id];
-var markerPosition = myMarker.getPosition();
-map.setCenter(markerPosition);
-google.maps.event.trigger(myMarker, 'click');
+    if (Markers[marker_id]) {
+        var myMarker = Markers[marker_id];
+        var markerPosition = myMarker.getPosition();
+        map.setCenter(markerPosition);
+        google.maps.event.trigger(myMarker, 'click');
+    }
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
 </script>
 <?php include 'footer.php'; ?>
