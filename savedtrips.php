6143a0e1d672ce5e6e4dfbfcb6ae90e455d11098
wesly-zhou <weslyz1026@gmail.com>
2024-05-01 08:40:02 -0400

    Update savedtrips.php

--- savedtrips.php
+++ savedtrips.php
@@ -77,7 +77,7 @@
     }
 
     function initMap(x) {
-        var tripIndex = x;
+        var tripIndex = x + 1;
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 0, lng: 0},
             zoom: 8
