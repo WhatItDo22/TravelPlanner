a83d421fcd55c711b22650e54c6fffcadc64c939
wesly-zhou <weslyz1026@gmail.com>
2024-05-01 00:03:39 -0400

    Update savedtrips.php

--- savedtrips.php
+++ savedtrips.php
@@ -32,6 +32,16 @@
             }
         }
         $conn->close();
+        $db2 = "dbbucggrkugs9b";
+        $tripNum = $_SESSION["tripNum"];
+        $conn2 = new mysqli($server, $dbusername, $dbpassword, $db2);
+        if ($conn2->connect_error) {
+            die("Connection failed: " . $conn2->connect_error);
+          }
+        $sql2 = "SELECT latitude, longitude FROM waypoints WHERE username = $username AND tripID = $tripNum";
+
+
+        $conn2->close();
     ?>
     <?php include 'footer.php'; ?>
     <script>
@@ -46,30 +56,4 @@
         }
     </script>
 </body>
-</html>
-
-<?php
-    session_start();
-    $title = "ItineraEase | Saved Route";
-?>
-<?php include 'header.php'; ?>
-    <div class="page-header">
-        <h1 class="page-header-title">Route</h1>
-    </div>
-    <?php
-        $server = "localhost";
-        $username = "upjomg4jsiwwg";
-        $password = "533%3611n_4`";
-        $db = "dbbucggrkugs9b";
-        $tripNum = $_SESSION["tripNum"];
-        $conn_event = new mysqli($server, $dbusername, $dbpassword, $db);
-        if ($conn_event->connect_error) {
-            die("Connection failed: " . $conn_event->connect_error);
-          }
-        $sql = "SELECT * FROM waypoints WHERE tripID = $tripNum";
-
-        $conn->close();
-        ?>
-    <?php include 'footer.php'; ?>
-    </body>
 </html>
