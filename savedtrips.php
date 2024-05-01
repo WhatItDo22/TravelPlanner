4b38e6d9e2cc72ddd09e114c66523cdd3031271b
AKaspal <111235859+WhatItDo22@users.noreply.github.com>
2024-05-01 06:48:35 -0400

    Update savedtrips.php

--- savedtrips.php
+++ savedtrips.php
@@ -51,7 +51,7 @@ if (isset($_POST['btn-route'])) {
     if ($conn->connect_error) {
         die('Connection failed: ' . $conn->connect_error);
     }
-    $sql = "SELECT latitude, longitude FROM waypoints WHERE username = '$username' AND tripID = $tripID ORDER BY waypointID";
+$sql = "SELECT latitude, longitude FROM waypoints WHERE username = '$username' AND tripID = $tripID ORDER BY ID";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
